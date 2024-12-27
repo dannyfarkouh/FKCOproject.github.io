809a566f4eeffb0cd76ff5d97db9f32c1ad43630
Danny Farkouh <dfarkouh@gmail.com>
2024-12-27 18:01:52 -0500

    Working navigation bar with no dropdown menu yet

---
+++ script.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('.nav');
+
+window.addEventListener('scroll', navEdit);
+
+function navEdit(){
+    if ( window.scrollY > nav.offsetHeight + 150 ){
+        nav.classList.add('active');
+    } else {
+        nav.classList.remove('active');
+    }
+}
