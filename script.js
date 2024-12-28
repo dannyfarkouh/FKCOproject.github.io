f00fd08432eab66100a604a8484333df0ed712e5
Danny Farkouh <dfarkouh@gmail.com>
2024-12-28 15:47:04 -0500

    Navbar is now fully functional

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const nav = document.querySelector('.nav');
 
-window.addEventListener('scroll', navEdit);
-function navEdit(){
+const about = document.querySelector('.about');
+const navContent = document.querySelector('.nav-content');
+
+const rotate = document.querySelector('.rotate');
+
+window.addEventListener('scroll', function navEdit(){
     if ( window.scrollY > nav.offsetHeight + 150 ){
         nav.classList.add('active');
     } else {
         nav.classList.remove('active');
     }
-}
+});
+
+about.addEventListener('click', function dropdownAppear(){
+    navContent.classList.toggle('show');
+});
+
+about.addEventListener('click', function (){
+    rotate.classList.toggle("down");
+});
