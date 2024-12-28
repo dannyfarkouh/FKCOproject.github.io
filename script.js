49b7cc70b1230e2d900881d903e416e96f5d624b
Danny Farkouh <dfarkouh@gmail.com>
2024-12-27 21:56:25 -0500

    navbar works on hover. I want it to work for click. that will be the next step...

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const nav = document.querySelector('.nav');
 
 window.addEventListener('scroll', navEdit);
-
 function navEdit(){
     if ( window.scrollY > nav.offsetHeight + 150 ){
         nav.classList.add('active');
