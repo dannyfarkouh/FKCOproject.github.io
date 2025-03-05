7d1a755dc91147ae45b610b31b7ff950ee914b84
Danny Farkouh <dfarkouh@gmail.com>
2025-03-04 21:09:45 -0500

    added blurring effect to main page at loading

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
+// reactive navbar
 const nav = document.querySelector('.nav');
-
 const about = document.querySelector('.about');
 const navContent = document.querySelector('.nav-content');
-
 const rotate = document.querySelector('.rotate');
-
 window.addEventListener('scroll', function navEdit(){
     if ( window.scrollY > nav.offsetHeight + 150 ){
         nav.classList.add('active');
@@ -20,3 +18,24 @@ about.addEventListener('click', function dropdownAppear(){
 about.addEventListener('click', function (){
     rotate.classList.toggle("down");
 });
+
+// blurring effect at loading 
+const bg = document.querySelector('.bg');
+
+let load = 0; 
+
+let int = setInterval(blurring, 10);
+
+function blurring(){
+    load++
+
+    if ( load > 99 ){
+        clearInterval(int)
+    }
+
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}; 
